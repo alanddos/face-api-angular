c0c261530768cbfea65fe258bb3ba2ad7ee15c00
Dev <alanddos>
2019-03-03 17:03:47 -0400

    colocanndo nomes

--- src/app/webcam-detect/webcam-detect.component.ts
+++ src/app/webcam-detect/webcam-detect.component.ts
@@ -14,10 +14,12 @@ declare var faceDetectionControls: any;
 })
 export class WebcamDetectComponent implements OnInit {
 
+  objeto = []
   constructor() {
     console.log(faceapi)
-    faceapi.loadSsdMobilenetv1Model('assets/')
+    //faceapi.loadSsdMobilenetv1Model('assets/')
     faceapi.loadMtcnnModel('assets/')
+    faceapi.loadFaceRecognitionModel('assets/')
   }
 
   ngOnInit() {
@@ -103,5 +105,82 @@ export class WebcamDetectComponent implements OnInit {
   //   console.log(options)
 
   // }
+
+  async start() {
+    const options = await this.getFaceDetectorOptions()
+    const videoEl = $('#video').get(0)
+
+    ////Localiza faces
+    const mtcnnResults = await faceapi.mtcnn(videoEl, options)
+
+    if (mtcnnResults) {
+      $('#overlay').height(videoEl.getAttribute('heigth'))
+      $('#overlay').width(videoEl.getAttribute('width'))
+      this.drawDetections(videoEl, $('#overlay').get(0), mtcnnResults.map(res => res.detection))
+      ////extrai as faces
+      const canvases = await faceapi.extractFaces(videoEl, mtcnnResults.map(res => res.alignedRect.box))
+      ////extrai os descritores
+      if (canvases != undefined && canvases != [] && canvases.length > 0) {
+        const descriptor = await faceapi.computeFaceDescriptor(canvases)
+
+        let labeledFaceDescriptors = new faceapi.LabeledFaceDescriptors('Alan ddos', descriptor)
+        if (labeledFaceDescriptors) {
+          if (this.objeto['descricao']) {
+            this.objeto['descricao'].push(labeledFaceDescriptors)
+          } else {
+            this.objeto['descricao'] = []
+            this.objeto['descricao'].push(labeledFaceDescriptors)
+          }
+        }
+        console.log(labeledFaceDescriptors)
+        console.log(descriptor)
+        console.log(this.objeto)
+        ////Se tiver mais de uma pessoa sera um array de 128 cada index da const descriptor
+
+        ////miniatura do rosto
+        var detectArea = $('#detectedFaces').get(0);
+
+        for (let index = 0; index < canvases.length; index++) {
+          const element = canvases[index];
+
+          var canvas = document.createElement('canvas');
+          let ctx = canvas.getContext("2d");
+          canvas.id = String(this.objeto['descricao'].length);
+          canvas.width = 100;
+          canvas.height = 100;
+          //canvas.style.zIndex = 8;
+          //canvas.style.position = "absolute";
+          canvas.style.border = "1px solid";
+          ctx.drawImage(element, 0, 0, 100, 100);
+          ctx.font = 'italic 14pt Calibri';
+          ctx.fillText(this.objeto['id'], 10, 95);
+          detectArea.prepend(canvas)
+          if (this.objeto['canvas']) {
+            this.objeto['canvas'].push(canvas)
+          } else {
+            this.objeto['canvas'] = []
+            this.objeto['canvas'].push(canvas)
+          }
+        }
+      }
+    } else {
+      ////esconde o overlay caso n encontre ninguem
+      $('#overlay').height(0)
+      $('#overlay').width(0)
+    }
+
+    // for (let index = 0; index < mtcnnResults.length; index++) {
+    //   const element = mtcnnResults[index];
+
+
+
+    //   console.log(element.detection._score)
+    //   if (element.detection._score > 0.8) {
+    //     console.log('=======================================')  
+    //   }
+    // }
+
+  }
+
 
 }
